03f1b7a0847c4c57aca10650d3350c5e263ccbe7
marcossch <schapiramarcos@gmail.com>
2017-06-03 21:16:08 +0200

    cambio puntos de art

--- PuntosDeArticulacion/dfs.py
+++ PuntosDeArticulacion/dfs.py
@@ -15,101 +15,61 @@ class DFS(object):
             - Grafo : Objeto de clase Graph.
             - Vertice : Vertice del grafo recibido.
         """
+    
         result = Graph(True)
         cantVert = len(self.grafo.get_vertices().keys())
         visitados = [False]*cantVert
         padres = [None]*cantVert
         stack = [self.initVert]
+        puntosDeArticulacion = []
+        low = [0]*cantVert
+        verticeAcutal = None
 
         k = 0
         for i in xrange(0, self.grafo.tam):
+
             vertice = stack.pop()
+            
             if not visitados[int(vertice)]:
+            
                 k += 1
                 result.aniadir_vertice(vertice, k)
+                low[int(vertice)] = k
+            
                 if padres[int(vertice)] is not None:
                     result.unir_vertices(padres[int(vertice)], vertice, 0)
 
                 visitados[int(vertice)] = True
-
                 auxStack = []
+
                 verticesVecinos = self.grafo.obtener_conocidos(vertice)
                 for verticeVecino in verticesVecinos:
+                
                     if not visitados[int(verticeVecino)]:
-                        auxStack.append(verticeVecino)
+                
                         padres[int(verticeVecino)] = vertice
+                        auxStack.append(verticeVecino)
+                    
+                    else:
+                        if( padres[int(vertice)] != verticeVecino ):
+                            low[int(vertice)] = min( low[int(vertice)], result.obtener_dato(verticeVecino) )
+                    
+                    if (verticeAcutal == vertice):
+
+                        if( (low[int(verticeVecino)] >= result.obtener_dato(vertice) ) and (vertice not in puntosDeArticulacion)):    
+                            puntosDeArticulacion.append(vertice)
+                        low[int(vertice)] = min( low[int(vertice)], low[int(verticeVecino)] )
+                
                 while len(auxStack) > 0:
-                    stack.append(auxStack.pop())
+                    stack.append(auxStack.pop())  
 
-        return result, padres
+                verticeAcutal = vertice          
 
-    def getLowList(self, padres, grafoTemp):
         
-        cantVert = len(self.grafo.get_vertices().keys())
-        visitados = [False]*cantVert
-        stack = [self.initVert]
-        low = [0]*cantVert
-
-        for i in xrange(0, self.grafo.tam):
-            vertice = stack.pop()
-            if not visitados[int(vertice)]:
-
-                visitados[int(vertice)] = True
-
-                auxStack = []
-                verticesVecinos = self.grafo.obtener_conocidos(vertice)
-
-                #se define el minimo low como el de si mismo
-                low [int(vertice)] = grafoTemp.obtener_dato(vertice)
-
-                for verticeVecino in verticesVecinos:
-                    
-                    if not visitados[int(verticeVecino)]:
-            
-                        distanciaVecino = grafoTemp.obtener_dato(verticeVecino)
-                        low[int(vertice)] = min(low[int(vertice)], low[int(verticeVecino)])
-                        
-                        auxStack.append(verticeVecino)
-                    
-                    elif (verticeVecino != padres[int(vertice)]):
-                            distanciaVecino = grafoTemp.obtener_dato(verticeVecino)
-                            low[int(vertice)] = min(low[int(vertice)], distanciaVecino)        
-
-                while len(auxStack) > 0:
-                    stack.append(auxStack.pop())
-
-        return low
-
-    def getPuntosDeArticulacion (self, padres, grafoTemp, low):
-            
-        cantVert = len(self.grafo.get_vertices().keys())
-        visitados = [False]*cantVert
-        stack = [self.initVert]
-        puntosDeArticulacion = []
-
-        for i in xrange(0, self.grafo.tam):
-            vertice = stack.pop()
-            if not visitados[int(vertice)]:
-
-                visitados[int(vertice)] = True
-
-                auxStack = []
-                verticesVecinos = self.grafo.obtener_conocidos(vertice)
-
-                for verticeVecino in verticesVecinos:
-                    if not visitados[int(verticeVecino)]:
-
-                        auxStack.append(verticeVecino)
-
-                        if ( (len(verticesVecinos) >= 2) and (padres[int(vertice)] == None) and (vertice not in puntosDeArticulacion)):
-                            #Todo vertice que es raiz con dos hijos o mas
-                            #es punto de articulacion
-                            puntosDeArticulacion.append(vertice)
-
-                        if( (padres[int(vertice)] != None) and (low[int(verticeVecino)] >= grafoTemp.obtener_dato(vertice)) and (vertice not in puntosDeArticulacion)):    
-                            puntosDeArticulacion.append(vertice)
-
-                while len(auxStack) > 0:
-                    stack.append(auxStack.pop())
+        verticesVecinosDFS = result.obtener_conocidos(self.initVert)
+        if ( (len(verticesVecinosDFS) >= 2)):
+            #Todo vertice que es la raiz del DFS con dos hijos o mas
+            #es punto de articulacion
+            puntosDeArticulacion.append(self.initVert)
 
         return puntosDeArticulacion
--- PuntosDeArticulacion/tarjan.py
+++ PuntosDeArticulacion/tarjan.py
@@ -15,15 +15,7 @@ class Tarjan(object):
         #Creador de grafos dfs
         dfs = DFS(vertice, self.grafo) 
 
-        #se obtiene el DFS del grafo. Este guarda en el valor de cada vertice un 
-        #tiempo de descubrimiento. Tambien se obtiene una lista con los padres 
-        #de cada vertice
-        grafoDFS, padres = dfs.getGrafoDFS()
-
-        #se obtiene una lista con el lowpoint de cada vertice 
-        lowList = dfs.getLowList(padres, grafoDFS)
-
-        #se obtienen los puntos de articulacion del grafo original
-        puntosArticulacion = dfs.getPuntosDeArticulacion(padres, grafoDFS, lowList)
+        #se obtienen los puntos de articulacion del DFS
+        puntosArticulacion = dfs.getPuntosDeArticulacion()
 
         return puntosArticulacion
